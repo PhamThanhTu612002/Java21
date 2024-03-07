881dcebeadb57e12c2fc353ed28a23119f13036e
Phạm Thanh Tú <phamthanhtu.migitek@gmail.com>
2024-03-07 21:47:32 +0700

    Booking Hotel Project

---
+++ SpringBoot/Spring_Boot/booking-hotel/src/test/java/vn/techmaster/bookinghotel/BookingHotelApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.techmaster.bookinghotel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookingHotelApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
